53cd5d8646f04dcd383bfbd42c4f296448b1d0e7
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-01-04 12:00:00 +0100

    Feature: Implemented an overview for customers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import DashBoard from '../views/Dashboard.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import DashBoard from "../views/Dashboard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'dashboard',
-      component: DashBoard
-    }
-  ]
-})
+      path: "/",
+      name: "dashboard",
+      component: DashBoard,
+    },
+    {
+      path: "/users",
+      name: "users",
+      component: () => import("../views/UserOverview.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/UserOverview.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="grid mt-8">
+    <DataTable
+      :value="customers"
+      :paginator="true"
+      :rows="10"
+      paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+      :rowsPerPageOptions="[10, 20, 50]"
+      responsiveLayout="scroll"
+      currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+    >
+      <Column field="name" header="Name"></Column>
+      <Column field="country.name" header="Country"></Column>
+      <Column field="company" header="Company"></Column>
+      <Column field="representative.name" header="Representative"></Column>
+      <template #paginatorstart>
+        <Button type="button" icon="pi pi-refresh" class="p-button-text" />
+      </template>
+      <template #paginatorend>
+        <Button type="button" icon="pi pi-cloud" class="p-button-text" />
+      </template>
+    </DataTable>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import CustomerService from "./service/CustomerService";
+import Button from "primevue/button";
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+
+export default {
+  components: {
+    Button,
+    Column,
+    DataTable,
+  },
+  setup() {
+    onMounted(() => {
+      customerService.value
+        .getCustomersLarge()
+        .then((data) => (customers.value = data));
+    });
+
+    const customers = ref();
+    const customerService = ref(new CustomerService());
+
+    return { customers };
+  },
+};
+</script>
