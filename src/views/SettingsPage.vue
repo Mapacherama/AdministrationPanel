3b8977802eba1672e3c1710a7d7d14a5d14218f9
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-01-19 11:33:48 +0100

    Feature: Final design changes

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -4,8 +4,10 @@
       <div class="card margin">
         <h1>Settings</h1>
         <div class="card">
-          <h1>Account</h1>
-          <p>Here you can change all the account settings.</p>
+          <h1 style="text-align: center">Account</h1>
+          <p style="text-align: center">
+            Here you can change all the account settings.
+          </p>
           <div class="formgrid grid">
             <div class="field col-12 md:col-6">
               <label for="email">Email</label>
@@ -74,45 +76,46 @@
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="grid">
+      <div class="col-12">
         <div class="card">
-          <h2>Change password</h2>
-          <div class="formgrid grid">
-            <div class="grid">
-              <div class="field col-12 md:col-12">
-                <label for="oldpassword">Old password</label> <br />
-                <Password
-                  v-model="oldPassword"
-                  aria-describedby="username-help"
-                  toggleMask
-                />
-                <br />
-                <br />
-                <small id="username-help"
-                  >Enter your old password to reset your password.</small
-                >
-                <br />
-                <br />
-              </div>
+          <h2 style="text-align: center">Change password</h2>
+          <div class="formgrid">
+            <div class="field col-12 md:col-12 align-middle">
+              <label for="oldpassword">Old password</label> <br />
+              <Password
+                v-model="oldPassword"
+                aria-describedby="username-help"
+                toggleMask
+              />
+              <br />
+              <br />
+              <small id="username-help"
+                >Enter your old password to reset your password.</small
+              >
+              <br />
+            </div>
 
-              <div class="field col-12 md:col-12">
-                <label for="oldpassword">New password</label> <br />
-                <Password class="w-full" v-model="value2" />
-                <br />
-              </div>
+            <div class="field col-12 md:col-12">
+              <label for="oldpassword">New password</label> <br />
+              <Password class="w-full" v-model="value2" />
+              <br />
+            </div>
+            <div class="field col-12 md:col-12 mb-6">
+              <label for="oldpassword">New password confirmation</label>
+              <br />
+              <Password class="w-full" v-model="value2" />
               <div class="field col-12 md:col-12 mb-6">
-                <label for="oldpassword">New password confirmation</label>
-                <br />
-                <Password class="w-full" v-model="value2" />
-                <div class="field col-12 md:col-12 mb-6">
-                  <Button
-                    class="p-button-primary p-button-rounded ml-8 mt-2"
-                    name="information"
-                    label="Opslaan"
-                    icon="pi pi-save"
-                    iconPos="left"
-                    type="submit"
-                  />
-                </div>
+                <Button
+                  class="p-button-primary p-button-rounded ml-8 mt-2"
+                  name="information"
+                  label="Opslaan"
+                  icon="pi pi-save"
+                  iconPos="left"
+                  type="submit"
+                />
               </div>
             </div>
           </div>
