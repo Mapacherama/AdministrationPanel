8893c72e4b11c1415456017411280abdc422766e
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-01-19 10:44:48 +0100

    Feature: Design for password reset

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -74,7 +74,64 @@
             </div>
           </div>
         </div>
+        <div class="card">
+          <h2>Change password</h2>
+          <div class="formgrid grid">
+            <div class="grid">
+              <div class="field col-12 md:col-12">
+                <label for="oldpassword">Old password</label> <br />
+                <Password
+                  v-model="oldPassword"
+                  aria-describedby="username-help"
+                  toggleMask
+                />
+                <br />
+                <br />
+                <small id="username-help"
+                  >Enter your old password to reset your password.</small
+                >
+                <br />
+                <br />
+              </div>
+
+              <div class="field col-12 md:col-4">
+                <label for="oldpassword">New password</label> <br />
+                <Password class="w-full" v-model="value2" />
+                <br />
+              </div>
+              <div class="field col-12 md:col-4 mb-6">
+                <label for="oldpassword">New password confirmation</label>
+                <br />
+                <Password class="w-full" v-model="value2" />
+                <Button
+                  class="p-button-primary p-button-rounded ml-8 mt-2"
+                  name="information"
+                  label="Opslaan"
+                  icon="pi pi-save"
+                  iconPos="left"
+                  type="submit"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import Password from "primevue/password";
+import Button from "primevue/button";
+
+export default {
+  components: {
+    Password,
+    Button,
+  },
+  data() {
+    return {
+      oldPassword: "",
+    };
+  },
+};
+</script>
