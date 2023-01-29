b2ecd3d12f0030bc3f90b94c688347529a675c9a
JeromeTesselaar <jerome.tesselaar@hva.nl>
2023-01-29 11:45:20 +0100

    Feature: The Charts are centered correctly.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,63 +1,50 @@
 <template>
-  <div className="mt-8">    
-      <div>
-        <h1>Dashboard</h1>
-      </div>     
-      <div class="grid d-flex">
-        <div class="col-4">   
-      <DeviceUsageChart /> 
-    
+  <div style="margin-top: 100px;" class="ml-4">
+    <div>
+      <h1 >Dashboard</h1>
+    </div>    
+    <div class="grid">
+      <div class="col-6">
+        <DeviceUsageChart />
+      </div>
+      <div class="col-6">
+        <VersionUsageChart class="chart" />
+        <StackedBarChart class="chart" />
       </div>
     </div>
-  <div class="grid">   
-    <div class="chart-container-row"> 
-    <div class="col-6">
-  <VersionUsageChart class="chart" />
-</div>
-<div class="col-6">
-  <StackedBarChart class="chart" />
-</div>
-</div>
-</div>
-
-</div>
-
-  
+  </div>
 </template>
 
 <script setup>
-import DeviceUsageChart from "../components/DeviceUsageChart.vue";
-import VersionUsageChart from "../components/VersionUsageChart.vue";
-import StackedBarChart from "../components/StackedBarChart.vue";
+  import DeviceUsageChart from "../components/DeviceUsageChart.vue";
+  import VersionUsageChart from "../components/VersionUsageChart.vue";
+  import StackedBarChart from "../components/StackedBarChart.vue";
 </script>
 
 <style scoped>
-.chart-container {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 15px;
-  gap: 5px;
-  flex-direction: column;
-}
-
-.chart-container-row {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0 15px;
-  gap: 5px;
-  flex-direction: row;
-}
-.chart-smaller{
-  width: 25%;
-}
-
-.grid {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.chart {
-  width: 100%;
-}
+  .chart-container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    gap: 5px;
+    padding: 0 15px;
+  }
+  .chart-container-row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 5px;
+    padding: 0 15px;
+  }
+  .grid {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+  .chart {
+    width: 100%;
+  }
+  .chart-smaller {
+    width: 25%;
+  }
 </style>
