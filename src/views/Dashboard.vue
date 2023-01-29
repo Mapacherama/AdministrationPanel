b5d5d9b0bb4b5aaa852d52b1ee46f3d6136c9b85
JeromeTesselaar <jerome.tesselaar@hva.nl>
2023-01-29 19:29:34 +0100

    Feature: More design changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,7 +4,7 @@
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <div>
-      <h1 >Dashboard</h1>
+      <h1 class="primary-header" >Dashboard</h1>
     </div>    
     <div class="grid">
       <div class="col-4">
